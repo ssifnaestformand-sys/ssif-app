3e15a5322a866010a809ca9e66105a0e8ddb6cc7
ssifnaestformand-sys <ssifnaestformand@sejssvejbaek-if.dk>
2026-05-05 23:03:09 +0200

    Update login.php
    Test- debug

--- login.php
+++ login.php
@@ -2,11 +2,14 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+$email = $_POST['Email'] ?? 'IKKE MODTAGET';
+$password = $_POST['Adgangskode'] ?? 'IKKE MODTAGET';
+
 $data = http_build_query([
     'Version' => '2',
     'Log_ind_med' => 'email',
-    'Email' => $_POST['email'],
-    'Adgangskode' => $_POST['password'],
+    'Email' => $email,
+    'Adgangskode' => $password,
     'Foreningsid' => '1031'
 ]);
 
@@ -14,8 +17,13 @@ $ch = curl_init('https://www.conventus.dk/dataudv/api/medlemslogin/login.php');
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result;
+echo json_encode([
+    'debug_email' => $email,
+    'debug_password' => $password,
+    'conventus_svar' => json_decode($result)
+]);
 ?>
