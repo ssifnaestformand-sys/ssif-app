48b4aafbba2bbd0a02713685c440276403c70a74
ssifnaestformand-sys <ssifnaestformand@sejssvejbaek-if.dk>
2026-05-07 14:03:04 +0200

    Sikkerhed.

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcwhIEVpgKS1ArEWh6QWp0WBXjQQB4RHM",
-  authDomain: "ssif-app.firebaseapp.com",
-  projectId: "ssif-app",
-  storageBucket: "ssif-app.firebasestorage.app",
-  messagingSenderId: "152071726664",
-  appId: "1:152071726664:web:295552fda118fbd7211a7d",
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
