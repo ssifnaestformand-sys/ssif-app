906f526861632e5405150c7212c9d4dfeab5a81f
ssifnaestformand-sys <ssifnaestformand@sejssvejbaek-if.dk>
2026-05-07 21:31:05 +0200

    fjern fra rod.

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main:  resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html'),
+        admin: resolve(__dirname, 'admin/index.html'),
       },
       output: {
         manualChunks: {
